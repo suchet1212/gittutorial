4d31d9fb3637ac890591ed4e6ad57d55a9eb3355
suchet1212 <suchetreddy45@gmail.com>
2023-02-28 13:02:18 +0530

    deleted:    demo.js 	new file:   dom.js 	new file:   index.html

---
+++ dom.js
@@ -0,0 +1,8 @@
+//console.log(document.all);
+let t= document.getElementsByClassName('title');
+
+t[0].style.FontWeight='bold';
+t[0].style.color='green';
+console.log(t[0]);
+let header = document.getElementById('main-header');
+header.style.borderBottom='solid 3px black';
